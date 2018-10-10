3f6dbf28431e00b2c660d3d3a7c8381b03c8e393
iot2020 <sigtrancoder@outlook.com>
2018-10-10 15:02:34 +0800

    1st

---
+++ js/restutils.js
@@ -0,0 +1,127 @@
+
+function Base64URLDecode(base64UrlEncodedValue) {
+    var result1;
+    var result2;
+    var newValue = base64UrlEncodedValue.replace("-", "+").replace("_", "/");
+
+    try {
+        result1 = window.atob(newValue);
+        result2 = decodeURIComponent(escape(window.atob(newValue)));
+        if (result1 !== result2) {
+           console.log(['_trackEvent', 'error_prevented', 'unicode decode']);
+        }
+    } catch (e) {
+        throw "Base64URL decode of JWT segment failed";
+    }
+
+    return result2;
+}
+
+function decodeJWT(jwt) {
+    var segments = jwt.split('.');
+
+    if (jwt === "") {
+        return "";
+    }
+
+    if (segments.length !== 3) {
+        throw "JWT is required to have three segments";
+    }
+
+    var header = Base64URLDecode(segments[0]);
+    var jwtInfo = Base64URLDecode(segments[1]);
+	
+	console.log(header);
+	console.log(jwtInfo);
+
+    sessionStorage.setItem('jwt_token_decoded', (jwtInfo));
+}
+
+function login() {
+    var usr=document.getElementById("txtUserName").value;
+    var pwd=document.getElementById("pwd").value;
+    sessionStorage.removeItem('jwt_token');
+    sessionStorage.removeItem('refresh_token');
+    sessionStorage.removeItem('jwt_token_decoded');
+    $.ajax({
+	    type: "POST",
+        url: "/api/auth/login" ,
+        dataType: "json",				
+        data: JSON.stringify({username:usr, password:pwd}),
+	    contentType: "application/json",
+        success: function (result) { 
+            if (result.token) {
+                sessionStorage.setItem('jwt_token', result.token);
+                sessionStorage.setItem('refresh_token', result.refreshToken);
+                decodeJWT(result.token);
+                alert("SUCCESS");
+			    window.location = "index.html";
+            }
+            ;
+        },
+        error : function() {
+            alert("异常！");
+        }
+    });
+}
+
+function login_info() {
+	console.log(sessionStorage.getItem('token'));
+	console.log(sessionStorage.getItem('jwt'));
+}
+
+function updateAndValidateToken(token, prefix, notify) {
+    var valid = false;
+    var tokenData = decodeJWT(token);
+    var issuedAt = tokenData.iat;
+    var expTime = tokenData.exp;
+    if (issuedAt && expTime) {
+        var ttl = expTime - issuedAt;
+        if (ttl > 0) {
+            var clientExpiration = new Date().valueOf() + ttl*1000;
+            store.set(prefix, token);
+            store.set(prefix + '_expiration', clientExpiration);
+            valid = true;
+        }
+    }
+    if (!valid && notify) {
+        window.location = "login.html";
+    }
+}
+
+function getDevices(limit, success_fun, error_fun) {
+  var devicesUrl = '/api/tenant/devices?limit='+limit;
+  var token = sessionStorage.getItem('jwt_token');
+  $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: devicesUrl,
+      headers: {'X-Authorization': 'Bearer '+token},
+      success: function(data) {
+		 success_fun(data);
+      },
+      error:function(data){
+        error_fun(data);
+        alert("error");
+      }
+  });
+}
+
+function getNextPageDevices (limit, idOffset, textOffset, success_fun, error_fun) {
+  var devicesUrl = '/api/tenant/devices?limit='+limit+'&idOffset='+idOffset+'&textOffse='+textOffset;
+  var token = sessionStorage.getItem('jwt_token');
+  var resp;
+  $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: devicesUrl,
+      headers: {'X-Authorization': 'Bearer '+token},
+      success: function(data) {
+		 success_fun(data);
+      },
+      error:function(data){
+        error_fun(data);
+        alert("error");
+      }
+  });
+}
